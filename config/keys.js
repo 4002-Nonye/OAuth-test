4229df506abf4c34b329c5da930fbda932ad91b1
4002-Nonye <royaltysungbin@gmail.com>
2025-02-20 15:42:47 +0100

    fix file path

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
   // we are in production - return prod keys
-  module.exports = require('.prod');
+  module.exports = require('./prod');
 } else {
   // we are in development - return dev keys
   module.exports = require('./keys');

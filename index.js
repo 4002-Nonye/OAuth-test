618f3105da0929b449232bdf844cd74ca875b726
4002-Nonye <royaltysungbin@gmail.com>
2025-02-20 15:29:22 +0100

    set up passport

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 const express = require('express');
+const passport = require('passport');
+const TwitterStrategy = require('passport-twitter').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
+
+// SETTING UP THE STRATEGY
+passport.use(
+  new TwitterStrategy(
+    {
+      consumerKey: keys.twitterConsumerAPIKey,
+      consumerSecret: keys.twitterConsumerAPISecret,
+      callbackURL: '/auth/x/callback',
+    },
+    (token, tokenSecret, profile) => {
+      console.log(profile);
+    }
+  )
+);
+app.get(
+  'auth/x',
+  passport.authenticate('twitter', {
+    scope: ['profile', 'email'],
+  })
+);
+
+app.get('/auth/x/callback', passport.authenticate('twitter'));
 
 app.get('/', (req, res) => {
   res.send({
@@ -8,8 +33,6 @@ app.get('/', (req, res) => {
     stack: 'Node js with express',
   });
 });
-
-app.get('/auth/facebook')
 
 const PORT = 8000;
 app.listen(PORT, () => console.log('Listening on port 8000'));
